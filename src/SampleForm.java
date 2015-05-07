772f8bb071993508e46048b8c3225b3a1eeb7143
Mitchell Valine <mitchellvaline@yahoo.com>
2015-05-07 13:22:53 -0700

    basic game complete

--- src/SampleForm.java
+++ src/SampleForm.java
@@ -35,6 +35,25 @@ public class SampleForm extends JFrame {
             }
         });
 
+        rollDiceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    SampleForm.this.rollDice(Integer.parseInt(betText.getText()));
+                }
+                catch(NumberFormatException e){
+                    JOptionPane.showMessageDialog(null, "Enter positive integers only");
+                }
+                catch(Exception e){
+                    JOptionPane.showMessageDialog(null, "An unknown error occurred, please try again");
+                }
+                finally {
+                    betText.setText("");
+                }
+
+            }
+        });
+
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -45,7 +64,48 @@ public class SampleForm extends JFrame {
     }
 
     public void newGame() {
-        total = Integer.parseInt(JOptionPane.showInputDialog("Enter total betting $"));
-        totalLabel.setText("$" + Integer.toString(total));
+        try {
+            total = Integer.parseInt(JOptionPane.showInputDialog("Enter total betting $"));
+            totalLabel.setText("$" + Integer.toString(total));
+        }
+        catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Enter positive integers only");
+            newGame();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "An unknown error occurred, please try again");
+            newGame();
+        }
+    }
+
+    public void rollDice(int bet) {
+        if(bet > 0 && bet <= total) {
+            Dice dice1 = new Dice();
+            Dice dice2 = new Dice();
+
+            dice1.rollDice();
+            dice2.rollDice();
+
+            int result = dice1.getValue() + dice2.getValue();
+
+            String message;
+            if (result == 7 || result == 11) {
+                total = total + (bet * 3);
+                message = "Congratulations, You rolled a " + Integer.toString(result)
+                        + ". You won: $" + Integer.toString(bet * 3);
+            } else {
+                total = (total - bet);
+                message = "Sorry, You rolled a " + Integer.toString(result);
+            }
+            JOptionPane.showMessageDialog(null, message);
+
+            totalLabel.setText("$" + Integer.toString(total));
+        }
+        else if(bet > total){
+            JOptionPane.showMessageDialog(null, "You don't have that much money");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Enter a positive integer");
+        }
     }
 }
